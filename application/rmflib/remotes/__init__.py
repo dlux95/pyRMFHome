37ff85f9b5cfba1828511102629fb54cf668be92
Daniel Laube <github@dlaube.de>
2016-05-25 15:55:05 +0200

    Added Debug messages to the remote loader

--- application/rmflib/remotes/__init__.py
+++ application/rmflib/remotes/__init__.py
@@ -3,7 +3,9 @@ try:
     from GPIORemote import GPIORemote
     #RPi usable, use GPIO Remote
     remote = GPIORemote()
+    print "Using GPIO Remote"
 except:
     #No RPI Usable, use Remote without functionality
     from Remote import Remote
     remote = Remote()
+    print "Using Debug Remote"
